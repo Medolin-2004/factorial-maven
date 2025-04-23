40b2ce6644a7cfc73fdef91ca0667cc30eda3e1c
Medolin-2004 <medolin.502236@sxcce.edu.in>
2025-04-23 14:09:11 +0530

    Add files via upload

---
+++ mavenproject2/src/main/java/Test.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author IT20
+ */
+public class Test {
+     static int factorial(int n)
+    {
+        int res = 1, i;
+        for (i = 2; i <= n; i++)
+            res *= i;
+        return res;
+    }
+
+    // main method
+    public static void main(String[] args)
+    {
+        int num = 5;
+        System.out.println("Factorial of " + num + " is "
+                           + factorial(5));
+    }
+    
+}
